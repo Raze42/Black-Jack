9b77215a22a8ef17fdbdbae8ba153cfef633b3ab
Raze42 <Raze42@ROGUE>
2022-07-08 23:59:14 -0700

    version 0.1

--- Black Jack/Black_Jack.py
+++ Black Jack/Black_Jack.py
@@ -27,73 +27,77 @@ def pick_card():
     card = value+suit    
     return card
 
-def add_cards(card1, card2):
-    value1 = 0
-    value2 = 0
-#Determine Value of Card 1
-    if (card1[0] == "A"):
-        value1 = 11
-    elif (card1[0] == "J" or card1[0] == "Q" or card1[0] == "K"):
-        value1 = 10
-    else:
-#       :-1 is supposed to return all but the last character in the string
-        value1 = int(card1[:-1])
-#Determine Value of Card 2
-    if (card2[0] == "A"):
-        value2 = 11
-    elif (card2[0] == "J" or card2[0] == "Q" or card2[0] == "K"):
-        value2 = 10
-    else:
-#       :-1 is supposed to return all but the last character in the string
-        value2 = int(card2[:-1])
-    return value1 + value2
-
-def clear_screen():
-    if(os.name == 'posix'):
-        os.system('clear')
-    # else screen will be cleared for windows
-    else:
-        os.system('cls')
+def add_cards(card):
+    total = 0
+    cardval = [x[:-1] for x in card]
+    for i in cardval:
+        if (i == "A"):
+            total = total + 11
+        elif (i == "J" or i == "Q" or i == "K"):
+            total = total + 10
+        elif (i == "B"):
+            total = total + 0
+        else:
+            #:-1 is supposed to return all but the last character in the string
+            total = total + int(i)
+    return total
 
 #Start of main program
-#clear_screen()
-print(chr(27) + "[2J")
 play = "yes"
 input("Welcome to Lame-o-Black Jack. Press any key to begin...")
-while play == "yes":
-    #clear_screen()
-    print("\033[H\033[J")
+while play == "yes" or play == "y" or play == "Y":
     print("Shuffling the deck...")
     print("Dealing cards...")
     #Initial Player Cards
-    player_card1 = pick_card()
-    player_card2 = pick_card()
-    while player_card1 == player_card2:
-        player_card2 = pick_card()
-    print("Player Cards:")
-    print("Player Card 1: " + player_card1)
-    print("Player Card 2: " + player_card2)
+    player_cards = [pick_card(), pick_card()]
+    while player_cards[0] == player_cards[1]:
+        player_cards[1] = pick_card()
+    print("Player Cards: ", player_cards)
     #Initial Dealer Cards
-    dealer_card1 = pick_card()
-    while dealer_card1 == player_card1:
-        dealer_card1 = pick_card()
-    while dealer_card1 == player_card2:
-        dealer_card1 = pick_card()
-    dealer_card2 = pick_card()
-    while dealer_card2 == player_card1 or dealer_card2 == player_card2 or dealer_card2 == dealer_card1:
-        dealer_card2 = pick_card()
-    print("Dealer Cards:")
-    print("Dealer Card 1: " + dealer_card1)
-    print("Dealer Card 2: Hidden" + dealer_card2)
-
-    if (add_cards(dealer_card1, dealer_card2) == 21):
-        if (add_cards(player_card1, player_card2) == 21):
+    dealer_cards = [pick_card(), pick_card()]
+    while dealer_cards[0] == player_cards[0]:
+        dealer_cards[0] = pick_card()
+    while dealer_cards[0] == player_cards[1]:
+        dealer_cards[0] = pick_card()
+    while dealer_cards[1] == player_cards[0] or dealer_cards[1] == player_cards[1] or dealer_cards[1] == dealer_cards[0]:
+        dealer_cards[1] = pick_card()
+    print("Dealer Cards: ", dealer_cards)
+    #Check for Initial Black Jack
+    if (add_cards(dealer_cards) == 21):
+        if (add_cards(player_cards) == 21):
             print("You and the dealer both have Black Jack! Push!")
         else:
             print("Dealer has Black Jack! You lose!")
-    elif (add_cards(player_card1, player_card2) == 21):
+    elif (add_cards(player_cards) == 21):
         print("You have Black Jack! You win!")
-    else: #begin asking for action
-        print("Player Card Total:" , add_cards(player_card1, player_card2))
-        play=input("Would you like to play again? (yes/no): ")
+    else: #No initial black-jack so begin asking for action
+        print("Player Card Total: " , add_cards(player_cards))
+        #print("Dealer Total: " , (add_cards(dealer_cards) - int(dealer_cards[1][:-1]))) #doesn't work because J,Q,K,A throw it off
+        action = input("Hit(h) or Stand(s)?: ")
+        
+        while action == "h" or action == "H" or action == "hit" or action == "Hit" or action == "HIT":
+            player_cards.append(pick_card())
+            print(player_cards)
+            print("Player Card Total: " , add_cards(player_cards))
+            if add_cards(player_cards) > 21:
+                print("Player busts! Dealer Wins.")
+                break
+            action = input("Hit(h) or Stand(s)?: ")
+        print("Revealing dealer cards...")
+        print("Dealer Cards: ", dealer_cards)
+        print("Dealer Card Total: " , add_cards(dealer_cards))
+        while (add_cards(dealer_cards) < 16):
+            dealer_cards.append(pick_card())
+            print("Dealer Cards: ", dealer_cards)
+            print("Dealer Card Total: " , add_cards(dealer_cards))
+        if add_cards(dealer_cards) > 21:
+            print("Dealer busts. Player wins.")
+        elif add_cards(dealer_cards) == add_cards(player_cards):
+            print("Push")
+        elif add_cards(dealer_cards) > add_cards(player_cards):
+            print("Dealer Wins")
+        elif add_cards(dealer_cards) < add_cards(player_cards):
+            print ("Player Wins")
+
+        play = input("Would you like to play again? (yes/no): ")
         
